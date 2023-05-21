1a6e1410d21fefc61a2c64ef4fb42bd77450006d
jeevanshib <jeevanshi.jb@gmail.com>
2023-05-21 12:41:58 +0530

    routes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,25 @@
-exports.home = (req, res) => {
-    res.send('Hello from Express');
-}
+const User = require("../models/user");
+
+exports.register = async(req, res) => {
+    const{
+        first_name,
+        last_name,
+        username,
+        email,
+        password,
+        bYear,
+        bDate,
+        gender
+    }= req.body;
+    const user = new User({
+        first_name,
+        last_name,
+        username,
+        email,
+        password,
+        bYear,
+        bDate,
+        gender
+    }).save();
+    res.json(user);
+};
