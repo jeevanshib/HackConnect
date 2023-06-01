4eeb93e1566841ebe2081b9f3e7e4abc690b6e01
jeevanshib <jeevanshi.jb@gmail.com>
2023-06-02 01:37:19 +0530

    Story section done

--- Frontend/src/pages/home/index.js
+++ Frontend/src/pages/home/index.js
@@ -1,23 +1,20 @@
 import Header from "../../components/header";
 import LeftHome from "../../components/home/left";
-// import useClickOutside from "../../helpers/clickOutSide";
-// import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
-import Stories from "../../components/home/stories";
 import RightHome from "../../components/home/right";
-import { useRef } from "react";
+import Stories from "../../components/home/stories";
+import "./style.css";
 
 export default function Home(){
-  const middle = useRef(null);
   const {user} = useSelector((user) => ({...user}));
 
 
   return (
-    <div>
+    <div className="home">
       <Header />
       <LeftHome user={user}/>
       <div className="absolute mt-[75px] left-[50%] translate-x-[-50%] home_middle">
-      <Stories />
+        <Stories />
       </div>
       <RightHome />
     </div>
