dd2115f32395c90e017276a67ecf42634ccd9399
jeevanshib <jeevanshi.jb@gmail.com>
2023-06-08 18:34:58 +0530

    post display

--- Frontend/src/components/post/CreateComment.js
+++ Frontend/src/components/post/CreateComment.js
@@ -38,6 +38,7 @@ export default function CreateComment({ user }) {
       return;
     }
 
+    
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = (event) => {
