970ad9da3687d34f67af6c5a780b53ddab1e45a0
jeevanshib <jeevanshi.jb@gmail.com>
2023-06-13 12:05:10 +0530

    profile backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {register} = require('../controllers/user');
 const {activateAccount} = require('../controllers/user');
 const {login, sendVerification, findUser, sendResetPasswordCode, validateResetCode
-, changePassword} = require('../controllers/user');
+, changePassword, getProfile} = require('../controllers/user');
 const {auth} = require('../controllers/user');
 const { authUser } = require('../middleware/auth');
 
@@ -11,14 +11,13 @@ const router = express.Router();
 
 
 router.post('/register', register);
-
 router.post('/activate',authUser, activateAccount);
-
 router.post('/login', login);
-
 router.post("/sendVerification", authUser, sendVerification);
 router.post("/findUser", findUser);
 router.post("/sendResetPasswordCode", sendResetPasswordCode);
 router.post("/validateResetCode", validateResetCode);
 router.post("/changePassword", changePassword);
+router.get("/getProfile/:username", getProfile);
+
 module.exports = router;
